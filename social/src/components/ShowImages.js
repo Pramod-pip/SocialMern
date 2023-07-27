78a70e9b71614915d0a4a93246bd7a939355e47b
Pramod Koppu <pramod.koppu@excelra.com>
2023-07-27 13:03:46 +0530

    changes to Image Grid

--- social/src/components/ShowImages.js
+++ social/src/components/ShowImages.js
@@ -1,7 +1,6 @@
 import React from 'react'
-import {
-    Grid
-  } from "@mui/material";
+import { Grid } from "@mui/material";
+import './ShowImages.css';
 
 export const ShowImages = (images) => {
     if (images.length === 1) {
@@ -10,10 +9,8 @@ export const ShowImages = (images) => {
             container
             justify="center"
             style={{
-              // backgroundImage: `http://localhost:5000/images/${image[0]}`,
               backgroundSize: "cover",
               width: "100%",
-              // marginLeft: 20,
               backgroundRepeat: "no-repeat",
               backgroundPosition: "center",
             }}
@@ -38,11 +35,10 @@ export const ShowImages = (images) => {
                       direction="row"
                       justify="center"
                       style={{
-                        // backgroundImage: `http://localhost:5000/images/${image}`,
                         width: "auto",
                         height: 400,
                         borderRadius: 5,
-                        marginBottom: 5,
+                        marginBottom: 2,
                         backgroundSize: "cover",
                         backgroundRepeat: "no-repeat",
                         backgroundPosition: "center",
@@ -69,8 +65,6 @@ export const ShowImages = (images) => {
                 direction="row"
                 justify="center"
                 style={{
-                  // backgroundImage: `url(${images[0]}`,
-                  // width: 250,
                   height: 500,
                   borderRadius: 5,
                   backgroundSize: "cover",
@@ -91,10 +85,8 @@ export const ShowImages = (images) => {
                         direction="row"
                         justify="center"
                         style={{
-                          // backgroundImage: `url(${image}`,
                           width: "auto",
                           borderRadius: 5,
-                          marginBottom: 5,
                           backgroundSize: "cover",
                           backgroundRepeat: "no-repeat",
                           backgroundPosition: "center",
@@ -104,6 +96,63 @@ export const ShowImages = (images) => {
                         // }}
                       >
                         <img src={`http://localhost:5000/images/${image}`} height="245px" alt="primary"/>
+                      </Grid>
+                    );
+                  }
+                
+              })}
+            </Grid>
+          </Grid>
+        );
+      }
+
+      if (images.length > 3) {
+        return (
+          <Grid container spacing={1}>
+            <Grid item md={6} lg={6} xl={6} xs={6} sm={6}>
+              <Grid
+                container
+                direction="row"
+                justify="center"
+                style={{
+                  height: 400,
+                  borderRadius: 5,
+                  backgroundSize: "cover",
+                  backgroundRepeat: "no-repeat",
+                  backgroundPosition: "center",
+                }}
+                // onDoubleClick={() => {
+                //   setShowImage(images[0]);
+                // }}
+              > <img src={`http://localhost:5000/images/${images[0]}`} alt="primary"/></Grid>
+            </Grid>{" "}
+            <Grid item md={6} lg={6} xl={6} xs={6} sm={6}>
+              {images.map((image, index) => {
+                if (index !== 0) {
+                    return (
+                      <Grid
+                        container
+                        direction="row"
+                        justify="center"
+                        style={{
+                          width: "auto",
+                          borderRadius: 5,
+                          backgroundSize: "cover",
+                          backgroundRepeat: "no-repeat",
+                          backgroundPosition: "center",
+                          position: 'relative',
+                        }}
+                        // onDoubleClick={() => {
+                        //   setShowImage(image);
+                        // }}
+                      >
+                        { index ===1 && <img src={`http://localhost:5000/images/${image}`} height="245px" alt="primary"/>}
+                        {index === 2 &&
+                        <>
+                        <img src={`http://localhost:5000/images/${image}`} style={{filter: "brightness(25%)"}} height="245px" alt="primary"/>
+                         <span className='overlayText'> + {images.length - 3} More</span>
+                         </>
+                         }
                       </Grid>
                     );
                   }
